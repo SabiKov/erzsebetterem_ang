ced167c95eb1195e769352caa01ad5f389e35939
Szabolcs Kovacs <szabolcs.kovacs@trl.hu>
2023-01-02 23:21:07 +0100

    init project strcture

---
+++ erzsebetterem/src/main/java/com/sabikov/erzsebetterem/ErzsebetteremApplication.java
@@ -0,0 +1,13 @@
+package com.sabikov.erzsebetterem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErzsebetteremApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErzsebetteremApplication.class, args);
+	}
+
+}
---
+++ erzsebetterem/src/test/java/com/sabikov/erzsebetterem/ErzsebetteremApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabikov.erzsebetterem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErzsebetteremApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
